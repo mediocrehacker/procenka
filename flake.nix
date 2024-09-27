2ae10e8273ccf97afe0649f40ef9e2b33996ca7f
mediocrehacker <mediocrehacker@protonmail.com>
2024-09-27 15:56:36 +0200

    Init repo

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Procenka application";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    purescript-overlay.url = "github:thomashoneyman/purescript-overlay";
+  };
+
+  outputs = { self, nixpkgs, purescript-overlay }:
+    let
+      name = "procenak";
+      system = builtins.currentSystem;
+      overlays = [ purescript-overlay.overlays.default ];
+      pkgs = import nixpkgs { inherit system overlays; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        name = name;
+
+        buildInputs = [
+          pkgs.esbuild
+          pkgs.nodejs_20
+          pkgs.nixpkgs-fmt
+          pkgs.purs
+          pkgs.purs-tidy
+          pkgs.purs-backend-es
+          pkgs.purescript-language-server
+          pkgs.spago-unstable
+        ];
+      };
+    };
+}
